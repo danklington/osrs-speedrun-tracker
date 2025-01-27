ad99f737c4e737b7c472b97a21a5947254ff7de2
dank <daniel.krause@34sp.com>
2025-01-27 19:59:29 +0100

    Further attempts to fix broken database connection.

--- db.py
+++ db.py
@@ -1,7 +1,9 @@
 from config import DB_CREDENTIALS
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, event
+from sqlalchemy.exc import OperationalError
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import scoped_session, sessionmaker
+import time
 
 # Create a connection to the database
 DB_USERNAME = DB_CREDENTIALS['DB_USERNAME']
@@ -21,6 +23,22 @@ engine = create_engine(
     pool_pre_ping=True,
     connect_args={'ssl': False}
 )
+
+# Retry connecting to the database if the connection is lost.
+def connect_with_retry(dbapi_connection, connection_record, connection_proxy):
+    retries = 5
+    for attempt in range(retries):
+        try:
+            dbapi_connection.ping()
+            break
+        except OperationalError:
+            if attempt < retries - 1:
+                time.sleep(2 ** attempt)
+            else:
+                raise
+
+event.listen(engine, 'engine_connect', connect_with_retry)
+
 
 Base = declarative_base()
 Base.metadata.bind = engine
